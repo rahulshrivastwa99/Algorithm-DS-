7b7d8d5320681cf06144c671b8e0db1b12067716
rahulshrivastwa99 <rahulshrivastwa1@gmail.com>
2025-08-31 10:47:08 +0530

    Starting Loops

---
+++ JavaIntro/Loops/Loops.java
@@ -0,0 +1,9 @@
+package JavaIntro.Loops;
+
+public class Loops {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 1000; ++i) {
+      System.out.println("Iteration: " + i);
+    }
+  }
+}
