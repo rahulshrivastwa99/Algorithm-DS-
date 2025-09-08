6151c4ff8e7c7f69ebc22d769135eb226177fea3
rahulshrivastwa99 <rahulshrivastwa1@gmail.com>
2025-09-08 12:56:27 +0530

    Array started

---
+++ DSA/Arrays/OutputInput.java
@@ -0,0 +1,43 @@
+package DSA.Arrays;
+
+import java.util.Scanner;
+
+public class OutputInput {
+  public static void main(String[] args) {
+    // System.out.println("Rahul");
+    // int[] arr = { 12, 34, 55, 67, 8, 90, 111 };
+    // System.out.println(arr.length);
+    // // for (int i = 0; i <= 5; i++) {
+    // // System.out.println(i + " " + arr[i]);
+    // // }
+    // for (int i = 0; i <= arr.length - 1; i++) {
+    // // System.out.println(arr[i]);
+    // System.out.println(i + " " + arr[i]);
+    // }
+    // // alternate approach:-
+    // for (int num : arr) {
+    // System.out.println(num);
+    // }
+
+    // for taking input in an array
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter the size of the array: ");
+    int size = sc.nextInt();
+
+    int[] list = new int[size];
+
+    System.out.println("Enter " + size + " elements:");
+    for (int i = 0; i < list.length; i++) {
+      list[i] = sc.nextInt();
+    }
+
+    // Printing the array elements
+    System.out.println("Array elements are:");
+    for (int i = 0; i < list.length; i++) {
+      System.out.println(i + " -> " + list[i]);
+    }
+
+    sc.close();
+  }
+}
---
+++ DSA/Arrays/array.java
@@ -0,0 +1,26 @@
+package DSA.Arrays;
+
+public class array {
+  public static void main(String[] args) {
+    // for intialiazation of an array
+    int[] arr = { 2, 3, 5, 56, 78, 99 };
+    // System.out.println(arr);
+    // indexing
+    System.out.println(arr[0]);
+    System.out.println(arr[1]);
+    System.out.println(arr[4]);
+    // updtaing -- mutability
+    arr[4] = 89;
+    System.out.println(arr[4]);
+
+    int[] x = new int[4]; // array is created and only 4 elemnts can be inserte din it.. means 4 size ka
+                          // array | 0 to 3 elements
+    x[0] = 12;
+    x[1] = 12;
+    x[2] = 12;
+    x[3] = 7782;
+    // x[4] = 12;
+    System.out.println(x[3]);
+
+  }
+}
