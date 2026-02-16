f60ff260e1748c6372c26bef3c81d0fa233669ed
rahulshrivastwa99 <rahulshrivastwa1@gmail.com>
2026-02-16 20:31:23 +0530

    Just working on 2darray

--- DSA/Arrays2D/TranposeofMatrix.java
+++ DSA/Arrays2D/TranposeofMatrix.java
@@ -10,11 +10,10 @@ public class TranposeofMatrix {
     int[][] transpose = new int[arr[0].length][arr.length];
     for (int i = 0; i < arr.length; i++) {
       for (int j = 0; j < arr[i].length; j++) {
-
         transpose[j][i] = arr[i][j];
       }
     }
-    System.out.println("Transpose of the matrix:");
+
     for (int i = 0; i < transpose.length; i++) {
       for (int j = 0; j < transpose[i].length; j++) {
         System.out.print(transpose[i][j] + " ");
@@ -23,3 +22,31 @@ public class TranposeofMatrix {
     }
   }
 }
+// package DSA.Arrays2D;
+
+// public class TranposeofMatrix {
+// public static void main(String[] args) {
+// int[][] arr = {
+// { 1, 2, 3 },
+// { 4, 5, 6 },
+// { 7, 8, 9 }
+// };
+
+// // Transpose using swap technique (only works for square matrices)
+// for (int i = 0; i < arr.length; i++) {
+// for (int j = i + 1; j < arr[i].length; j++) {
+// int temp = arr[i][j];
+// arr[i][j] = arr[j][i];
+// arr[j][i] = temp;
+// }
+// }
+
+// for (int i = 0; i < arr.length; i++) {
+// for (int j = 0; j < arr[i].length; j++) {
+// System.out.print(arr[i][j] + " ");
+// }
+// System.out.println();
+// }
+
+// }
+// }
---
+++ JavaIntro/Basic/program5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class program5 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the range of the Number: ");
+    int range = sc.nextInt();
+    System.out.println("Enter the numbers: ");
+    for (int i = 0; i < range; i++) {
+      int n = sc.nextInt();
+    }
+    System.out.println();
+    sc.close();
+
+  }
+}
