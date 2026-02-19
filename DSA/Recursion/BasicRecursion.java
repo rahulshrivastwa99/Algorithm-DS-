54ec1234f9e57003a13dd01aa9c3d5bdafe7ba11
rahulshrivastwa99 <rahulshrivastwa1@gmail.com>
2026-02-19 22:39:24 +0530

    Started RECURSION

---
+++ DSA/Recursion/BasicRecursion.java
@@ -0,0 +1,12 @@
+package DSA.Recursion;
+
+public class BasicRecursion {
+  public static void main(String[] args) {
+    rahul();
+  }
+
+  public static void rahul() {
+    System.out.println("Rahul");
+
+  }
+}
