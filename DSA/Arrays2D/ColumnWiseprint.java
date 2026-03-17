af58f0eb7895ec4e55893d20504cb55bdc235ba7
rahulshrivastwa99 <rahulshrivastwa1@gmail.com>
2026-03-17 23:10:20 +0530

    changes

--- DSA/Arrays2D/ColumnWiseprint.java
+++ DSA/Arrays2D/ColumnWiseprint.java
@@ -7,6 +7,11 @@ public class ColumnWiseprint {
         { 5, 6, 7, 8 },
         { 9, 10, 11, 12 }
     };
+    int[][] arr1 = {
+        { 1, 2, 3, 4 },
+        { 5, 6, 7, 8 },
+        { 9, 10, 11, 12 }
+    };
 
     System.out.println("Printing 2D array in column-wise manner");
     for (int j = 0; j < arr[0].length; j++) {
