a09d41b57a2d3232b16d55a3d86e656b10b3aba4
rahulshrivastwa99 <rahulshrivastwa1@gmail.com>
2026-05-08 18:44:17 +0530

    Incorrect the file minprint.java

--- DSA/Arrays/Minprint.java
+++ DSA/Arrays/Minprint.java
@@ -5,6 +5,9 @@ public class Minprint {
   public static void main(String[] args) {
     int[] arr = { -90, 1, 2, 3, 77, 4, 5, 6, 7, 8, 9 };
     int min = Integer.MAX_VALUE;
+    int min = Integer.MAX_VALUE;
+    int min = Integer.MAX_VALUE;
+    int min = Integer.MAX_VALUE;
     // int min = arr[0]; or you can use this also
     for (int i = 0; i < arr.length; i++) {
       if (arr[i] < min) {
